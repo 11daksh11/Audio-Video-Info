a79cb991224e20c27c167ed44179ec9623c4afff
Shirsha Datta <shirshadatta2000@gmail.com>
2020-10-01 18:24:03 +0530

    readme updated

--- lib/main.dart
+++ lib/main.dart
@@ -10,69 +10,6 @@ main() {
   runApp(VideoApp());
 }
 
-/*
-
-class VideoApp extends StatefulWidget {
-  @override
-  _VideoAppState createState() => _VideoAppState();
-}
-
-class _VideoAppState extends State<VideoApp> {
-  VideoPlayerController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.network(
-        'http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4')
-      ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {});
-      });
-  }
-
-  Widget build(BuildContext context) {
-    StatusBar.color(Colors.black12);
-    return MaterialApp(
-      home: Scaffold(
-      appBar: apps,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          ibody,
-          new Center(
-          child: _controller.value.initialized
-              ? AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
-                )
-              : Container(),
-        )
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _controller.value.isPlaying
-                  ? _controller.pause()
-                  : _controller.play();
-            });
-          },
-          child: Icon(
-            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-          ),
-        ),
-        ],
-      ),
-    ),
-    );
-  }
-@override
-void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
-}
-*/
-
 class VideoApp extends StatefulWidget {
   @override
   _VideoAppState createState() => _VideoAppState();
